ae94364163415b1dcd59ab0a4d8c46806e204b5b
Joaquim-Renato <jocajoaquimas@gmail.com>
2024-08-28 15:39:39 -0300

    test2

---
+++ progSimples.py
@@ -0,0 +1,8 @@
+# Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+
+# Solicita a idade do usuário
+idade = int(input("Digite sua idade: "))
+
+# Exibe uma mensagem personalizada
+print(f"Olá, {nome}! Você tem {idade} anos.")
